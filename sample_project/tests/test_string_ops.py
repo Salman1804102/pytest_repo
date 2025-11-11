0f9bc4ef5545a7fa338e8975aefea5a6a94e289f
Salman Farsi <62229105+Salman1804102@users.noreply.github.com>
2025-11-11 08:47:47 +0600

    Update test_string_ops.py

--- sample_project/tests/test_string_ops.py
+++ sample_project/tests/test_string_ops.py
@@ -1,6 +1,9 @@
-def reverse(s):
-    return s[::-1]
+from string_ops import reverse, to_upper
 
-def to_upper(s):
-    return s.upper()
+def test_reverse():
+    assert reverse("abc") == "cba"
+    assert reverse("") == ""
 
+def test_to_upper():
+    assert to_upper("abc") == "ABC"
+    assert to_upper("PyTest") == "PYTEST"
